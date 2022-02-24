aa99eb4375251b343bdb5f098338e6ffddc59740
srodrigo28 <rodrigoexer1@gmail.com>
2022-02-24 07:18:44 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { TarefasModule } from './tarefas'
+import { TarefasModule } from './tarefas';
 
 @NgModule({
   declarations: [
